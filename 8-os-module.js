b58064052a9cc8a6b359cda47fe6e5086499ccae
Zaheen <workwithzaheen@gmail.com>
2023-05-25 03:01:02 +0530

    small update

---
+++ 8-os-module.js
@@ -0,0 +1,20 @@
+const os = require("os");
+
+// info about current user
+const user = os.userInfo();
+// console.log(user);
+
+// method returns the system uptime in seconds
+// setInterval(() => {
+//   console.log(`The system uptime is ${os.uptime()} seconds`);
+// }, 1000);
+
+console.log(`The system uptime is ${os.uptime()} seconds`);
+
+const currentOS = {
+  name: os.type(),
+  release: os.release(),
+  totalMem: os.totalmem(),
+  freeMem: os.freemem(),
+};
+console.log(currentOS);
--- app.js
+++ app.js
@@ -9,4 +9,4 @@ const base = path.basename(filePath);
 console.log(base);
 
 const absolute = path.resolve(__dirname, "content", "subfolder", "test.txt");
-console.log(a);
+console.log(absolute);
