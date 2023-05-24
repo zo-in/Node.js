a6044f78611ed72e3bc34a33244cec33020f74e8
Zaheen <workwithzaheen@gmail.com>
2023-05-25 02:59:15 +0530

    Started Node.js

---
+++ 3-modules.js
@@ -0,0 +1,18 @@
+// const secret = "SUPER SECRET";
+// const john = "john";
+// const peter = "peter";
+
+// const sayHi = (name) => {
+//   console.log(`Hello there ${name}`);
+// };
+
+const names = require("./4-names");
+const sayHi = require("./5-utils");
+const data = require("./6-alternative-flavor");
+// console.log(data);
+
+require("./7-mind-grenade");
+// const {addValues} = require("./7-mind-grenade");
+// sayHi("susan");
+// sayHi(names.peter);
+// sayHi(names.john);
