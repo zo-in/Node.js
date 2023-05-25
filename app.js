ca126811115a63d7156313147da7fa5790e81e0c
Zaheen <workwithzaheen@gmail.com>
2023-05-26 00:13:24 +0530

    Set up .gitignore

--- app.js
+++ app.js
@@ -1,19 +1,5 @@
-const http = require("http");
+const _ = require("lodash");
 
-const server = http.createServer((req, res) => {
-  if (req.url === "/") {
-    res.end("Welcome to our home page");
-    return;
-  }
-  if (req.url === "/about") {
-    res.end("Here is our short history");
-    return;
-  }
-  res.end(`
-  <h1>Oops!</h1>
-  <p>We can't seem to find the page you are looking for</p>
-  <a href="/">backhome</a>
-  `);
-});
-
-server.listen(4000);
+const items = [1, [2, [3, [4]]]];
+const newItems = _.flattenDeep(items);
+console.log(newItems);
