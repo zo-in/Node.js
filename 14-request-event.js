6fa5448195c0a733c16c1d74eac1d70a7aa3b21f
Zaheen <workwithzaheen@gmail.com>
2023-05-27 18:53:34 +0530

    Node.js completed.

---
+++ 14-request-event.js
@@ -0,0 +1,12 @@
+const http = require("http");
+
+// const server = http.createServer((req, res) => {
+//   res.end('Welcome')
+// })
+
+const server = http.createServer();
+server.on("request", (req, res) => {
+  res.end("Welcome");
+});
+
+server.listen(5000);
--- app.js
+++ app.js
@@ -1,18 +1,1 @@
-const { readFile, writeFile } = require("fs").promises;
-
-const start = async () => {
-  try {
-    const first = await readFile("./content/first.txt", "utf8");
-    const second = await readFile("./content/second.txt", "utf8");
-    await writeFile(
-      "./content/result-mind-grenade",
-      `This is amazinggg! ${first} ${second}`,
-      { flag: "a" }
-    );
-    console.log(first, second);
-  } catch (error) {
-    console.log(err);
-  }
-};
-
-start();
+console.log("NODE COMPLETE!!!!");
